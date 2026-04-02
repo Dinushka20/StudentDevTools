ae4f156f519c651ab2fe99d19b7a3c5ace5dfc93
Dinushka20 <it23736450@my.sliit.lk>
2026-04-03 00:28:43 +0530

    Fix AdSense tag specifically for crawler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,12 +31,12 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
       <head>
-        <Script
+        <meta name="google-adsense-account" content="ca-pub-9779182285280660" />
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9779182285280660"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+        ></script>
       </head>
       <body className="min-h-full flex flex-col">
         <Navbar />
